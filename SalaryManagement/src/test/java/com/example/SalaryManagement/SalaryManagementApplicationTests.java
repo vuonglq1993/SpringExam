5901d41a4da9eb056106c60b9ab4d3544ff21338
bemixzyy <bolunlun1210@gmail.com>
2026-02-05 08:49:37 +0700

    Exam

---
+++ SalaryManagement/src/test/java/com/example/SalaryManagement/SalaryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SalaryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
